bd26f5b0de7eee913b66f079567f23ae994e87b2
Cameron McWilliam <cam.mcwilliam@me.com>
2020-03-23 13:21:52 +0000

    fixed push

--- ci/push.sh
+++ ci/push.sh
@@ -5,10 +5,11 @@ if [ -f $build_dir/image_list ]
 then
     while read line
     do
-        if [ "$line" != "" ] && ["$line" != *"index"*] && ["$line" != *"index"] && ["$line" == *":"*]
+        if [ "$line" != "" ] && ["$line" != *"index"*] && ["$line" != *"index"]
         then
             newImage="$line-$TRAVIS_CPU_ARCH"
             image_tag $line $newImage
+            docker rmi $line
             image_push $newImage
         fi
     done < $build_dir/image_list
