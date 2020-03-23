53efcc854ee7a2c622e427d0538525f511fd2664
Cameron McWilliam <cam.mcwilliam@me.com>
2020-03-23 15:46:19 +0000

    removed index from push

--- ci/push.sh
+++ ci/push.sh
@@ -5,12 +5,14 @@ if [ -f $build_dir/image_list ]
 then
     while read line
     do
-        if [ "$line" != "" ] && [ "$line" != *"index"* ] && [ "$line" != *"index" ]
+        if [[ $line != "" ]] && [[ ! $line =~ "index" ]] && [[ line =~ ":" ]]
         then
             newImage="$line-$TRAVIS_CPU_ARCH"
             image_tag $line $newImage
             docker rmi $line
             image_push $newImage
+            line=`echo $line | sed -e "s/$line/$newImage/"`
         fi
     done < $build_dir/image_list
+    cat $build_dir/image_list
 fi
